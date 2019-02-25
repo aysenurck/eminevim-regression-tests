2c9cf0dfd71810065eb9b7dd53c40998cfedbd3d
AYSENUR CELIK <aysenur.celik@eminevim.com.tr>
2019-02-25 21:44:59 +0300

    this project updated2

--- src/test/java/context/properties/SetProperties.java
+++ src/test/java/context/properties/SetProperties.java
@@ -29,9 +29,9 @@ public class SetProperties {
         if (Boolean.parseBoolean(System.getProperty("execute.local"))) {
             prop.setProperty("windows.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
         } else if (Boolean.parseBoolean(System.getProperty("execute.local"))) {
-            prop.setProperty("windows.chrome.driver", System.getProperty("user.home") + System.getProperty("driver.path"));
+            prop.setProperty("windows.chrome.driver", System.getProperty("jenkins.workspace") + System.getProperty("driver.path"));
         }
-        
+
         prop.setProperty("mac.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver");
         prop.setProperty("implicitly.wait", "30");
         prop.setProperty("wait.timeout.seconds", "30");
