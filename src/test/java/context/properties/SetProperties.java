a788f7df2d202ef80d8e5a015dcfdfbadf3bab26
AYSENUR CELIK <aysenur.celik@eminevim.com.tr>
2019-02-25 21:06:51 +0300

    this project updated2

--- src/test/java/context/properties/SetProperties.java
+++ src/test/java/context/properties/SetProperties.java
@@ -15,24 +15,25 @@ public class SetProperties {
 
     public void setProperties() throws Exception {
 
-        logger.info("har.file.path --> " + System.getProperty("user.dir") + "/src/test/resources/");
-        logger.info("galen.report.path --> " + System.getProperty("user.dir") + "/target/GalenLayoutReports/");
+        logger.info("har.file.path --> " + System.getProperty("user.dir") + "\\src\\test\\resources\\");
+        logger.info("galen.report.path --> " + System.getProperty("user.dir") + "\\target\\GalenLayoutReports\\");
         logger.info("take.a.video --> " + System.getProperties().getProperty("take.a.video"));
         logger.info("browser.type --> " + System.getProperties().getProperty("browser.type"));
+        logger.info("base.url --> " + System.getProperties().getProperty("base.url"));
 
         Properties prop = new Properties();
         OutputStream output = null;
 
         output = new FileOutputStream(filePath.getAbsolutePath());
 
-        prop.setProperty("windows.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
-        prop.setProperty("mac.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+        prop.setProperty("windows.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+        prop.setProperty("mac.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver");
         prop.setProperty("implicitly.wait", "30");
         prop.setProperty("wait.timeout.seconds", "30");
         prop.setProperty("page.load.timeout", "20");
         prop.setProperty("base.url", System.getProperty("base.url"));
-        prop.setProperty("har.file.path", System.getProperty("user.dir") + "/src/test/resources/");
-        prop.setProperty("galen.report.path", System.getProperty("user.dir") + "/target/GalenLayoutReports/");
+        prop.setProperty("har.file.path", System.getProperty("user.dir") + "\\src\\test\\resources/");
+        prop.setProperty("galen.report.path", System.getProperty("user.dir") + "\\target\\GalenLayoutReports/");
         prop.setProperty("take.a.video", System.getProperties().getProperty("take.a.video"));
         prop.setProperty("browser.type", System.getProperties().getProperty("browser.type"));
         prop.store(output, "NYX Costemics Regression Test Configuration");
